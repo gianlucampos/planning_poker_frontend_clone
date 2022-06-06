cb8a11465fc9b67aec0cddf30f8fbd40a235c9bf
Gianluca Campos <gianlucampos97@gmail.com>
2022-06-05 23:38:12 -0300

    Integrando rotação no componente

--- lib/widgets/jogador_widget.dart
+++ lib/widgets/jogador_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:planning_poker_flutter/widgets/rotate_widget.dart';
 
 class JogadorWidget extends StatefulWidget {
   final String nome;
@@ -13,6 +14,7 @@ class JogadorWidget extends StatefulWidget {
 
 class _JogadorWidgetState extends State<JogadorWidget> {
   bool isVotoExibido = false;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -21,7 +23,7 @@ class _JogadorWidgetState extends State<JogadorWidget> {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            isVotoExibido ? CardVotado() : CardBranco(),
+            RotateWidget(),
             SizedBox(height: 15),
             Text(
               widget.nome,
@@ -32,39 +34,4 @@ class _JogadorWidgetState extends State<JogadorWidget> {
     );
   }
 
-  void mostrarVoto() {
-    setState(() {
-      isVotoExibido = true;
-    });
-  }
-
-  Widget CardVotado() {
-    return Container(
-      width: 90,
-      height: 140,
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.blueAccent, width: 2),
-        borderRadius: BorderRadius.all(Radius.circular(20)),
-      ),
-      child: Center(
-        child: Text(
-          widget.voto,
-          textScaleFactor: 2,
-          style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
-        ),
-      ),
-    );
-  }
-
-  Widget CardBranco() {
-    return Container(
-      width: 90,
-      height: 140,
-      decoration: BoxDecoration(
-        color: Colors.grey[300],
-        border: Border.all(color: Colors.grey, width: 2),
-        borderRadius: BorderRadius.all(Radius.circular(20)),
-      ),
-    );
-  }
 }
--- lib/widgets/rotate_widget.dart
+++ lib/widgets/rotate_widget.dart
@@ -18,70 +18,57 @@ class _RotateWidgetState extends State<RotateWidget> {
   @override
   void initState() {
     super.initState();
-    // initialize();
   }
 
-  void initialize() {
-    if (_timer == null) {
-      _timer = Timer.periodic(const Duration(milliseconds: 5), (_) => start());
-      // _timer = Timer.periodic(const Duration(seconds: 1), (_) => start());
-    }
-  }
-
-  void start() {
+  void rotate() {
     angle = angle >= 2 ? 0 : angle;
     setState(() {
       angle += speed;
     });
-    print(angle.toString());
     if (angle >= 1) stop();
+  }
+
+  void start() {
+    if (_timer == null) {
+      _timer = Timer.periodic(const Duration(milliseconds: 5), (_) => rotate());
+    }
   }
 
   void stop() {
     _timer?.cancel();
     _timer = null;
-    // angle = 0;
+  }
+
+  void reset() {
+    super.setState(() {
+      angle = 0;
+    });
   }
 
   // 0.0 - 0.5 -> nao mostra
   // 0.5 - 1.5 -> mostra
-  bool isFrontImage(double ang) {
+  bool isBackImage(double ang) {
     return ang >= 0.5 && ang <= 1.5;
-    // return ang >= 0.05 && ang <= 0.15;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
+    return Center(
       child: Column(
         children: [
           TextButton(
-            onPressed: () {
-              initialize();
-            },
+            onPressed: start,
             child: Text("Start"),
           ),
           TextButton(
-            onPressed: () {
-              stop();
-            },
-            child: Text("Stop"),
-          ),
-          TextButton(
-            onPressed: () {
-              super.setState(() {
-                angle = 0;
-              });
-            },
+            onPressed: reset,
             child: Text("Reset"),
           ),
           buildTransform(),
-          Text('Joao')
           // buildTransform(),
         ],
       ),
-    ));
+    );
   }
 
   Transform buildTransform() {
@@ -93,29 +80,43 @@ class _RotateWidgetState extends State<RotateWidget> {
         child: Align(
             alignment: Alignment.center,
             child: Container(
-                color: const Color(0xffaaffff),
                 child: Transform(
-                  alignment: Alignment.center,
-                  transform: Matrix4.identity()..rotateY(Math.pi),
-                  child: simpleContainer(),
-                  // child: testingGradient(),
-                ))));
+                    alignment: Alignment.center,
+                    transform: Matrix4.identity()..rotateY(Math.pi),
+                    child: simpleContainer()))));
   }
 
-  Widget simpleContainer() {
-    return Column(
-      children: [
-        Container(
-          child: isFrontImage(angle)
-              ? Center(child: Text('P', textScaleFactor: 8))
-              : null,
-          height: 200,
-          width: 200,
-          color: Color(0xff74b3ff),
+  Widget CardVotado() {
+    return Container(
+      width: 90,
+      height: 140,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.blueAccent, width: 2),
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+      ),
+      child: Center(
+        child: Text(
+          'P',
+          textScaleFactor: 2,
+          style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
         ),
-      ],
+      ),
     );
   }
 
+  Widget CardBranco() {
+    return Container(
+      width: 90,
+      height: 140,
+      decoration: BoxDecoration(
+        color: Colors.grey[300],
+        border: Border.all(color: Colors.grey, width: 2),
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+      ),
+    );
+  }
 
+  Widget simpleContainer() {
+    return isBackImage(angle) ? CardVotado() : CardBranco();
+  }
 }
